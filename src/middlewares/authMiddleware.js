5ded8f014268437467652d7fad42b685023922b3
amrrkhaled <amr.khaledsaada@gmail.com>
2025-02-11 01:59:31 +0200

    finished auth middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import Redis from "ioredis";
+const SECRET_KEY = process.env.SECRET_KEY;
+
+const redisClient = new Redis(); 
+
+export const generateToken = (user) => {
+  return jwt.sign({ id: user.id, email: user.email, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+};
+
+export const verifyToken = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).send("Access denied. No token provided.");
+  }
+
+  // Check if token is blacklisted
+  const isBlacklisted = await redisClient.get(`blacklist:${token}`);
+  if (isBlacklisted) {
+    return res.status(401).send("Token is revoked. Please log in again.");
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).send("Invalid token");
+  }
+};
