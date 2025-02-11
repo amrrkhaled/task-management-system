b1e35ed9c9bafee6613a88b097a610043f294705
amrrkhaled <amr.khaledsaada@gmail.com>
2025-02-11 02:01:42 +0200

    modified entry point

--- src/server.js
+++ src/server.js
@@ -1,20 +1,41 @@
-const express = require("express");
-const cors = require("cors");
-const db = require("./models"); // Import models (Sequelize)
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { swaggerUi, swaggerSpec } from "./config/swagger.js";
+import authRoutes from "./routes/authRoutes.js";
+import db from "./models/index.js" // Ensure correct path to models
+
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// const taskRoutes = require("./routes/taskRoutes");
-// app.use("/tasks", taskRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+global.onlineUsers = new Map();
+app.use(authRoutes);
 
 const PORT = process.env.PORT || 5000;
+
 db.sequelize.sync({ alter: true }) // Change to `force: true` if you want to reset tables
   .then(() => {
     console.log("Database synchronized successfully.");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, () => {
+      const swaggerURL = `http://localhost:${PORT}/api-docs`;
+      console.log(`Server running on port ${PORT}`);
+      console.log(`Swagger Docs available at: ${swaggerURL}`);
+
+      setTimeout(() => {
+        import("./workers/emailWorker.js")
+          .then(() => console.log("Queue worker started"))
+          .catch((err) => console.error("Failed to start queue worker:", err));
+      }, 5000); // Start after 5 seconds
+   
+   
+   
+    });
   })
   .catch((err) => console.error("Database sync failed:", err));
-
-
