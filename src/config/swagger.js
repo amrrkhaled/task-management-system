db44929f586735e84c5c532e95da9cc2bac7ac41
amrrkhaled <amr.khaledsaada@gmail.com>
2025-02-13 17:58:37 +0200

    updated swagger config

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -14,7 +14,7 @@ const options = {
       version: "1.0.0",
       description: "API documentation",
     },
-    servers: [{ url: "http://localhost:5000" }],
+    servers: [{ url: "http://13.60.154.39:5000" }],
     components: {
       securitySchemes: {
         bearerAuth: {
