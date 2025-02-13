bbdf236e50a346272b63ddf63e690558b015452d
amrrkhaled <amr.khaledsaada@gmail.com>
2025-02-13 18:03:45 +0200

    cors allowed for domain

--- src/server.js
+++ src/server.js
@@ -11,13 +11,14 @@ const app = express();
 
 
 const corsOptions = {
-  origin: "*", // Change this to your frontend's domain for better security
+  origin: ["https://www.collabtask.me", "http://localhost:5000"], // ✅ Explicitly allow frontend domains
   methods: "GET,POST,PUT,DELETE,OPTIONS",
   allowedHeaders: "Content-Type,Authorization",
-  credentials: true
+  credentials: true, s
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
