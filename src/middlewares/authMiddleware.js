85c8e5a03ee40aedebde44cc21cd0ff25aa08be7
amrrkhaled <amr.khaledsaada@gmail.com>
2025-02-14 20:55:51 +0200

    added checkrole in middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
 import Redis from "ioredis";
 const SECRET_KEY = process.env.SECRET_KEY;
-
+import db from "../models/index.js";  
+const { Project, ProjectMember} = db;  
 const redisClient = new Redis(); 
 
 export const generateToken = (user) => {
@@ -29,5 +30,47 @@ export const verifyToken = async (req, res, next) => {
     next();
   } catch (err) {
     return res.status(401).send("Invalid token");
+  }
+};
+
+
+export const checkManager = async (req, res, next) => {
+  const { projectId } = req.params;
+  const userId = req.user.id;
+
+  try {
+    const project = await Project.findOne({ where: { id: projectId } });
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    if (project.managerId !== userId) {
+      return res.status(403).json({ message: "Access denied. Only the project manager can perform this action." });
+    }
+    next(); 
+  } catch (error) {
+    console.error("Error checking project manager:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const checkAdmin = async (req, res, next) => {
+  const { projectId } = req.params;
+  const userId = req.user.id;
+
+  try {
+    const projectMember = await ProjectMember.findOne({
+      where: { userId, projectId, role: "admin" },
+    });
+
+    if (!projectMember) {
+      return res.status(403).json({ message: "Access denied. Only admins can perform this action." });
+    }
+
+    next(); // Allow access if the user is an admin
+  } catch (error) {
+    console.error("Error checking project admin:", error);
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
