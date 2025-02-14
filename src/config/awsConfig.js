47de9a43c5480ec72d62806811b1eb88e7f644ad
amrrkhaled <amr.khaledsaada@gmail.com>
2025-02-14 20:55:28 +0200

    added s3 bucket config

--- src/config/awsConfig.js
+++ src/config/awsConfig.js
@@ -1,5 +1,6 @@
 import { SQSClient } from "@aws-sdk/client-sqs";
 import { SESClient } from "@aws-sdk/client-ses";
+import { S3Client } from "@aws-sdk/client-s3";
 
 const REGION = process.env.AWS_REGION || "eu-north-1";
 const ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
@@ -24,6 +25,11 @@ export const sesClient = new SESClient({
     accessKeyId: ACCESS_KEY_ID,
     secretAccessKey: SECRET_ACCESS_KEY,
   },
+});
+export const s3 = new S3Client({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
 });
 
 console.log("AWS REGION:", REGION);
